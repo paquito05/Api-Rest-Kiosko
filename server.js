b00d5470e66db7e6ee38cc5dd2caec9c7a1fd547
@ paquito05 <you@example.com>
2021-10-08 00:07:51 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.host = process.env.HOST || '0.0.0.0';
+        this.port = process.env.PORT || 3000;
+
+        this.usuariosPath = '/api/usuarios';
+        //this.authPath     = '/api/auth';
+
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicación
+        this.routes();
+    }
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+        
+        //this.app.use( this.authPath, require('./routes/auth'));
+        this.app.use( this.usuariosPath, require('./routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen( this.port, this.host, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
